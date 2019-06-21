b38a61a851753d25e49c46090e9db07703f2ab5e
Cameron <cameronleecalder@gmail.com>
2019-06-20 20:42:00 -0500

    New bracket template

---
+++ public/js/bracket.js
@@ -0,0 +1,29 @@
+/* eslint-disable indent */
+/* eslint-disable prettier/prettier */
+var saveData = {
+  teams: [
+      ["",""]
+    ],
+  results: [
+    [
+      []
+    ]
+  ]
+};
+
+function saveFn(data, userData) {
+  var json = jQuery.toJSON(data);
+  $("#saveOutput").text("POST " + userData + " " + json);
+}
+
+$(function() {
+  var container = $("div#save .demo");
+  container.bracket({
+    init: saveData,
+    save: saveFn,
+    userData: "http://myapi"
+  });
+
+  var data = container.bracket("data");
+  $("#dataOutput").text(jQuery.toJSON(data));
+});
